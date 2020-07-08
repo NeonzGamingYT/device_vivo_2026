af6bd4d635e9c0ac90675c3a05ed9ba3c3f20c98
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-08 18:58:54 +0530

    2026: Enable running_without_sync_framework
    - Fixes tint flickers in night-light
    
    Change-Id: Ic349e855c230ceb15303379a4a6a36d0b4a6de72
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -16,6 +16,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
+    debug.stagefright.ccodec=0
 
 # Performance
 PRODUCT_SYSTEM_PROPERTIES += \
@@ -31,6 +32,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.running_without_sync_framework=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
     debug.sf.enable_hwc_vds=0
