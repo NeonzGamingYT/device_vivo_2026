40a1fe06c5f87f1ae715579a055ac11b446c9cf5
Nishant Kumar <www.rajsonu13@gmail.com>
2022-02-20 09:08:08 +0530

    2026: Properties: Minor changes
    * Change PRODUCT_SYSTEM_DEFAULT_PROPERTIES to PRODUCT_SYSTEM_PROPERTIES as it is highly discouraged and will be deprecated by google
    * Change PRODUCT_PROPERTY_OVERRIDES to PRODUCT_PRODUCT_PROPERTIES as init reads /product/build.prop after /vendor/build.prop so we can override props set by /vendor/build.prop by specifying props in /product/build.prop
    
    Reference: - https://android.googlesource.com/platform/build/+/master/Changes.md#changes-in-system-properties-settings
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,18 +1,24 @@
 # Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Camera
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
     vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
 
+# Dexopt
+PRODUCT_SYSTEM_PROPERTIES += \
+    pm.dexopt.first-boot=quicken \
+    pm.dexopt.bg-dexopt=everything
+
 # OMX
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    debug.stagefright.ccodec=0
+PRODUCT_SYSTEM_PROPERTIES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
 
 # Performance
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
@@ -22,12 +28,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
 # Surfaceflinger
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    debug.sf.enable_hwc_vds=0
 
 # Zygote
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
