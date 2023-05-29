a2c65e9571d2a07ce92e66776e290aa657486212
R0rt1z2 <me@r0rt1z2.com>
2023-05-29 17:30:35 +0300

    2026: libshims: Shim another missing showlogo symbol

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -5,7 +5,14 @@
  */
 
 #include <ui/Size.h>
+#include <DisplayConfig.h>
 
 using namespace android;
 
 extern "C" ui::Size _ZN7android2ui4Size7INVALIDE{-1, -1};
+
+status_t _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(const sp<IBinder>& display, ui::DisplayMode* mode);
+
+status_t _ZN7android21SurfaceComposerClient22getActiveDisplayConfigERKNS_2spINS_7IBinderEEEPNS_13DisplayConfigE(const sp<IBinder>& display, DisplayConfig* config) {
+  return _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(display, (ui::DisplayMode*)config);
+}
